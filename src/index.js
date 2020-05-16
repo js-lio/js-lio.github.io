3fb8bbc9973f9f92ad3454a63fe0c3f62292ea69
junyong.hong <125173449@qq.com>
2020-05-16 22:34:07 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @description:
+ * @author: junyong.hong
+ * @createTime: 2020/4/23
+ * @version: 1.0.0.0
+ * @history:
+ *    1、
+ *    2、
+ *
+ */
+class FastCache{
+    constructor() {
+        this.list = {}
+    }
+    set(key, value) {
+        this.list[key] = value
+    }
+    get(key) {
+        return this.list[key]
+    }
+    clear() {
+        this.list = {}
+    }
+}
+
+window.FastCache = FastCache
